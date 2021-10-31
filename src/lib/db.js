075ec28f8fbff0b2d7a7b123d4f45f300dcb6db7
dylan99913 <dylan_wee@live.com>
2021-10-31 09:57:06 +0000

    email

---
+++ src/lib/db.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL="https://bhcnnjmjikydagaajxyk.supabase.co",
+  import.meta.env.VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTY3MjY5MCwiZXhwIjoxOTUxMjQ4NjkwfQ.7dPmcCmEdwObUXnjz9FYZrl0lueQ-6DuJvOYOgiTfqk"
+)
